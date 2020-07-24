9ad1bbc0a179c5ceab69850996cccbca61128be3
Minh Nhat Le <37999344+minhnhatvt@users.noreply.github.com>
2020-07-24 12:30:50 +0700

    update images inference

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -27,6 +27,7 @@ ROOT_DIR = os.path.abspath("../")
 sys.path.append(ROOT_DIR)  # To find local version of the library
 from mrcnn import utils
 
+import cv2 
 
 ############################################################
 #  Visualization
@@ -165,7 +166,76 @@ def display_instances(image, boxes, masks, class_ids, class_names,
     ax.imshow(masked_image.astype(np.uint8))
     if auto_show:
         plt.show()
+    return masked_image.astype(np.uint8)
 
+
+def draw_predicted_image(image, boxes, masks, class_ids, class_names,
+                      scores=None, show_mask=True, show_bbox=True,
+                      colors=None, captions=None, score_threshold=0.6):
+    # Number of instances
+    N = boxes.shape[0]
+    if not N:
+        pass
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
+
+
+
+    # Generate random colors
+    colors = colors or random_colors(N)
+
+
+    masked_image = image.astype(np.uint8).copy()
+    for i in range(N):
+        color = colors[i]
+
+        # Bounding box
+        if not np.any(boxes[i]):
+            # Skip this instance. Has no bbox. Likely lost in image cropping.
+            continue
+        
+        if not captions:
+            class_id = class_ids[i]
+            score = scores[i] if scores is not None else None
+            label = class_names[class_id]
+            caption = "{} {:.2f}".format(label, score) if score else label
+        else:
+            caption = captions[i]
+       
+        if (score < score_threshold):
+            continue
+
+        y1, x1, y2, x2 = boxes[i]
+   
+        if show_bbox:
+            masked_image = cv2.rectangle(masked_image, (x1, y1), (x2,y2), color, thickness=2)
+
+        # Label
+        
+       
+        cv2.putText(masked_image, caption, (x1, y1 + 8),cv2.FONT_HERSHEY_SIMPLEX  ,0.75, (255,255,255),2)
+        #ax.text(x1, y1 + 8, caption,
+                #color='w', size=11, backgroundcolor="none")
+
+        # Mask
+        mask = masks[:, :, i]
+        if show_mask:
+            masked_image = apply_mask(masked_image, mask, color)
+
+        # Mask Polygon
+        # Pad to ensure proper polygons for masks that touch image edges.
+        padded_mask = np.zeros(
+            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+        padded_mask[1:-1, 1:-1] = mask
+        contours = find_contours(padded_mask, 0.5)
+        for verts in contours:
+            # Subtract the padding and flip (y, x) to (x, y)
+            verts = np.fliplr(verts) - 1
+            p = Polygon(verts, facecolor="none", edgecolor=color)
+           
+
+ 
+    return masked_image.astype(np.uint8)
 
 def display_differences(image,
                         gt_box, gt_class_id, gt_mask,
